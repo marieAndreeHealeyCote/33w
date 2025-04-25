b9c84b092a15d5d0fe5b6e74add7a1c76a5c9f69
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-04-25 11:42:27 -0400

    ajout setting.json

---
+++ js/setting.js
@@ -0,0 +1,13 @@
+
+{
+  "liveSassCompile.settings.formats": [
+    {
+      "format": "expanded",
+      "extensionName": ".css",
+      "savePath": "~/.."
+    }
+  ],
+  "window.zoomLevel": 4,
+  "liveSassCompile.settings.autoprefix": [],
+  "liveServer.settings.donotShowInfoMsg": true
+}
