a204cf9d90091aa42f3a2fdf4bd395a959ca8cf4
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-06-14 01:18:29 -0400

    modification index.php et search.php pour mise en page

--- index.php
+++ index.php
@@ -7,17 +7,19 @@
 ?>
 <?php get_header() ?>
 <section class="populaire">
-    <?php if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-            // affiche l'image "mise en avant" miniature
-            the_post_thumbnail('thumbnail');
-    ?>
-            <!-- affiche le titre principal du "post" -->
-            <h1><?php the_title(); ?></h1>
-            <!-- // Cette fonction permet d'afficher l'ensemble du contenu du post (article ou page) -->
-    <?php the_content();
-        }
-    } ?>
+    <div class="populaire__single">
+        <?php if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                // affiche l'image "mise en avant" miniature
+                the_post_thumbnail('medium_large');
+        ?>
+                <!-- affiche le titre principal du "post" -->
+                <h1><?php the_title(); ?></h1>
+                <!-- // Cette fonction permet d'afficher l'ensemble du contenu du post (article ou page) -->
+        <?php the_content();
+            }
+        } ?>
+    </div>
 </section>
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -7,17 +7,30 @@
 ?>
 <?php get_header() ?>
 <section class="populaire">
-    <?php if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-            // affiche l'image "mise en avant" miniature
-            the_post_thumbnail('thumbnail');
-    ?>
-            <!-- affiche le titre principal du "post" -->
-            <h1><?php the_title(); ?></h1>
-            <!-- // Cette fonction permet d'afficher l'ensemble du contenu du post (article ou page) -->
-    <?php the_content();
-        }
-    } ?>
+    <div class="populaire__category">
+        <?php if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                // affiche l'image "mise en avant" miniature
+        ?>
+                <article class="populaire__category__contenu">
+                    <div class="populaire__category__image">
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                    </div>
+                    <div class="populaire__category__titre">
+                        <!-- affiche le titre principal du "post" -->
+                        <h2><?php the_title(); ?></h2>
+                        <!-- // Cette fonction permet d'afficher l'ensemble du contenu du post (article ou page) -->
+                        <?php
+                        // Cette fonction permet de tronquer et d'afficher un permalien
+                        $lien = ' [...] <a href="' . get_permalink() . '">Lire la suite</a>';
+                        echo '<p>' . wp_trim_words(get_the_excerpt(), 50, $lien) . '</p>';
+                        ?>
+                    </div>
+                </article>
+        <?php
+            }
+        } ?>
+    </div>
 </section>
 <?php get_footer(); ?>
