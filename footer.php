c48beee1c11ffaed16132e82b245667d5d536c3c
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-07-11 19:29:51 -0400

    correction de la vague au pied de page

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@ $footer_telephone = get_theme_mod('footer_telephone', 'Default Phone');
 ?>
 
 <footer class="piedpage">
-    <?php vague("#aaa", '#2e8b57'); ?>
+    <?php vague("#fff", '#2e8b57'); ?>
     <?php get_template_part("templates/piedpage"); ?>
 
     <style>
@@ -30,57 +30,58 @@ $footer_telephone = get_theme_mod('footer_telephone', 'Default Phone');
             color: <?= $footer_couleur ?>
         }
     </style>
-
-    <section class="piedpage__s1">
-        <div class="piedpage__s1__liens">
-            <h2>Liens sur le voyage</h2>
-            <ul class="piedpage__s1__liens__liste">
-                <li>
-                    <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien1 ?>" target="_blank"><?= $footer_text_lien1 ?></a>
-                </li>
-                <li>
-                    <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien2 ?>" target="_blank"><?= $footer_text_lien2 ?></a>
-                </li>
-                <li>
-                    <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien3 ?>" target="_blank"><?= $footer_text_lien3 ?></a>
-                </li>
-                <li>
-                    <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien4 ?>" target="_blank"><?= $footer_text_lien4 ?></a>
-                </li>
-            </ul>
-        </div>
-        <div class="piedpage__s1__adresse">
-            <div class="piedpage__s1__adresse__coord">
-                <h2>Adresse</h2>
-                <p> <?= $footer_adresse ?>
-                    <br />
-                    <?= $footer_telephone ?>
-                </p>
-                <?php get_search_form() ?>
+    <div class="piedpage-fond">
+        <section class="piedpage__s1">
+            <div class="piedpage__s1__liens">
+                <h2>Liens sur le voyage</h2>
+                <ul class="piedpage__s1__liens__liste">
+                    <li>
+                        <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien1 ?>" target="_blank"><?= $footer_text_lien1 ?></a>
+                    </li>
+                    <li>
+                        <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien2 ?>" target="_blank"><?= $footer_text_lien2 ?></a>
+                    </li>
+                    <li>
+                        <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien3 ?>" target="_blank"><?= $footer_text_lien3 ?></a>
+                    </li>
+                    <li>
+                        <a class="piedpage__s1__liens__liste-items" href="<?= $footer_url_lien4 ?>" target="_blank"><?= $footer_text_lien4 ?></a>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="piedpage__s1__description">
-            <h2>Mission du Club de Voyage</h2>
-            <p>
-                <?= $footer_description ?>
-            </p>
-        </div>
-    </section>
-    <section class="piedpage__s2">
-        <div class="piedpage__s2__medias-sociaux">
-            <?php icone_sociaux($footer_couleur) ?>
-        </div>
-        <div class="piedpage__s2__externe">
-            <?php wp_nav_menu(array(
-                "menu" => "externe",
-                'container' => '',
-                'menu_class' => '',
-            )); ?>
-        </div>
-    </section>
-    <section class="piedpage__s3">
-        <div>© Copyright Club de Voyage 2025</div>
-    </section>
+            <div class="piedpage__s1__adresse">
+                <div class="piedpage__s1__adresse__coord">
+                    <h2>Adresse</h2>
+                    <p> <?= $footer_adresse ?>
+                        <br />
+                        <?= $footer_telephone ?>
+                    </p>
+                    <?php get_search_form() ?>
+                </div>
+            </div>
+            <div class="piedpage__s1__description">
+                <h2>Mission du Club de Voyage</h2>
+                <p>
+                    <?= $footer_description ?>
+                </p>
+            </div>
+        </section>
+        <section class="piedpage__s2">
+            <div class="piedpage__s2__medias-sociaux">
+                <?php icone_sociaux($footer_couleur) ?>
+            </div>
+            <div class="piedpage__s2__externe">
+                <?php wp_nav_menu(array(
+                    "menu" => "externe",
+                    'container' => '',
+                    'menu_class' => '',
+                )); ?>
+            </div>
+        </section>
+        <section class="piedpage__s3">
+            <div>© Copyright Club de Voyage 2025</div>
+        </section>
+    </div>
 </footer>
 <script src="scripts/checkbox.js"></script>
 <?php wp_footer() ?>
--- front-page.php
+++ front-page.php
@@ -9,11 +9,11 @@
 <?php $hero_background = get_theme_mod("hero_background"); ?>
 <section class="hero" style="background-image: url('<?= $hero_background ?>');">
 
-    <?php
-    $hero_background[0] = get_theme_mod("hero_background_0");
-    $hero_background[1] = get_theme_mod("hero_background_1");
-    $hero_background[2] = get_theme_mod("hero_background_2");
-    ?>
+    <!-- <?php
+            $hero_background[0] = get_theme_mod("hero_background_0");
+            $hero_background[1] = get_theme_mod("hero_background_1");
+            $hero_background[2] = get_theme_mod("hero_background_2");
+            ?>
     <div class="carrousel" style="background-image: url('<?= $hero_background[0] ?>'); opacity:1"></div>
     <div class="carrousel" style="background-image: url('<?= $hero_background[1] ?>'); opacity:0"></div>
     <div class="carrousel" style="background-image: url('<?= $hero_background[2] ?>'); opacity:0"></div>
@@ -21,7 +21,7 @@
         <input type="radio" class="carrousel__radio" name="carrousel__radio">
         <input type="radio" class="carrousel__radio" name="carrousel__radio">
         <input type="radio" class="carrousel__radio" name="carrousel__radio">
-    </form>
+    </form> -->
 
 
     <?php get_template_part("templates/hero"); ?>
